0ea6343422414a3a22e2efe072a01023712d2b8b
tokimanana <tokimananasarobidy@gmail.com>
2025-09-16 23:49:33 +0300

    icon library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAweSomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'netflix-clone';
+
+  faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAweSomeIcons);
+  }
+
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import { IconDefinition } from "@fortawesome/free-brands-svg-icons"
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons"
+
+export const fontAweSomeIcons: IconDefinition[] = [
+  faCircleInfo
+];
