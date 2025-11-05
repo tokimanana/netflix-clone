904aa26474f4bd94c12faf52d222f5e052f22cbe
tokimanana <tokimananasarobidy@gmail.com>
2025-11-05 23:44:08 +0300

    movie-list: Souris avec molette : Maintenez Shift + molette

--- src/app/home/movie-selector/movie-list/movie-list.component.ts
+++ src/app/home/movie-selector/movie-list/movie-list.component.ts
@@ -12,32 +12,41 @@ export type Mode = 'GENRE' | 'TREND';
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss',
 })
-export class MovieListComponent implements OnInit{
-  @Input() genreId: number = -1;
-
+export class MovieListComponent implements OnInit {
+  @Input() genreId = -1;
   @Input() mode: Mode = 'GENRE';
 
-  private readonly tmdbService = inject(TmdbService);
+  tmdbService = inject(TmdbService);
 
   moviesByGenre: Movie[] | undefined;
+  trendMovies: Movie[] | undefined;
 
   constructor() {
     effect(() => {
-      if(this.mode === 'GENRE') {
-        const movieByGenreResponse =
-          this.tmdbService.moviesByGenre().value ?? {} as MovieApiResponse;
-        if(movieByGenreResponse.genreId === this.genreId) {
+      if (this.mode === 'GENRE') {
+        const movieByGenreResponse = this.tmdbService.moviesByGenre().value ?? {} as MovieApiResponse;
+        if (movieByGenreResponse.genreId === this.genreId) {
           this.moviesByGenre = movieByGenreResponse.results;
         }
+      } else if (this.mode === 'TREND') {
+        const trendingMoviesResponse = this.tmdbService.fetchtrendMovie().value;
+        if (trendingMoviesResponse) {
+          this.trendMovies = trendingMoviesResponse.results;
+        }
       }
-    })
+    });
   }
 
   ngOnInit(): void {
     this.fetchMoviesByGenre();
+    this.fetchTrends();
   }
 
   private fetchMoviesByGenre(): void {
-    this.tmdbService.getMoviesByGenre(this.genreId)
+    this.tmdbService.getMoviesByGenre(this.genreId);
+  }
+
+  private fetchTrends(): void {
+    this.tmdbService.getTrends();
   }
 }
